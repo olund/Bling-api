eca876149769fe0dfc51ad5757d83271f53f7bb5
Olund <henke.olund@gmail.com>
2015-12-02 14:40:34 +0100

    sample route for adding a test user

--- routes/users.js
+++ routes/users.js
@@ -3,18 +3,32 @@ var router = express.Router();
 
 var User = require('../models/user');
 
-var user = {
-    id: 1,
-    username: "test",
-    email: "test",
-    password: "test"
-};
+
 
 router.get('/', function(req, res) {
     User.find({}, function(err, users) {
         res.json(users);
     });
+});
 
+router.post('/add', function(req, res) {
+    var user = new User({
+        id: 1,
+        username: "test",
+        email: "test@asd.com",
+        password: "test",
+        birthday: Date.now(),
+        gender: "Female"
+    });
+
+    user.save(function (err) {
+        if (err) {
+            console.log(err);
+            res.json(err);
+        }
+        res.json({msg: "added"});
+        console.log("added");
+    });
 });
 
 module.exports = router;
